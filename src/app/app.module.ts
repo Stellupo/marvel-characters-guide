d4fcd604f76fa01410cd36343db755b5e254c5b6
Stellupo <e.braido@gmail.com>
2021-01-26 16:07:07 +0100

    Marvel characters guide : stage 1 (basic design)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CharactersComponent } from './characters/characters.component';
+import { CharacterDetailComponent } from './character-detail/character-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CharactersComponent,
+    CharacterDetailComponent
   ],
   imports: [
     BrowserModule
