0504ec97459490f0a4b9a48c2ec96fbc30c6a173
Stellupo <e.braido@gmail.com>
2021-01-28 15:07:19 +0100

    Marvel characters guide : stage 5 (header)

--- src/app/character.ts
+++ src/app/character.ts
@@ -5,4 +5,5 @@ export class Character{
   abilities: string[];
   group?: string;
   img: string;
+  header: string;
 }
--- src/app/mock-characters.ts
+++ src/app/mock-characters.ts
@@ -2,6 +2,19 @@ import {Character} from './character';
 
 
 export const CHARACTERS: Character[] = [
-  { id: 1, name: 'WOLVERINE', story: 'From the northern wilderness of Canada hails one of the gruffest, most irascible, totally cynical and brooding member of the X-Men ever to grace the team with his presence.', abilities: ['Indestructible claws', 'Healing powers', 'Enhanced senses'], group: 'X-men', img: 'assets/images/wolwerine.jpg'},
-  { id: 2, name: 'IRON MAN (Tony Stark)', story: 'Having created a wondrous suit of armor to keep himself alive, Tony has revised it dozens of times, each version with increased capabilities, faster operation, reduced energy usage, and adaptability to the greatest threats the universe has to offer.', abilities: ['360 vision', 'Energy blast', 'Powerful Armor'], group: 'Avengers', img: 'assets/images/Ironman.jpg' },
-  ];
+  { id: 1,
+    name: 'WOLVERINE',
+    story: 'From the northern wilderness of Canada hails one of the gruffest, most irascible, totally cynical and brooding member of the X-Men ever to grace the team with his presence.',
+    abilities: ['Indestructible claws', 'Healing powers', 'Enhanced senses'],
+    group: 'X-men',
+    img: 'assets/images/wolwerine.jpg',
+    header: 'assets/images/wolwerine_header.png'
+  },
+  { id: 2,
+    name: 'IRON MAN (Tony Stark)',
+    story: 'Having created a wondrous suit of armor to keep himself alive, Tony has revised it dozens of times, each version with increased capabilities, faster operation, reduced energy usage, and adaptability to the greatest threats the universe has to offer.',
+    abilities: ['360 vision', 'Energy blast', 'Powerful Armor'],
+    group: 'Avengers',
+    img: 'assets/images/Ironman.jpg',
+    header: 'assets/images/ironman_header.png'
+  }];
