3f12f401d460b9ba34b1e36678d0cc3ac931b2fb
Stellupo <e.braido@gmail.com>
2021-02-08 17:55:11 +0100

    Marvel characters guide : stage 14 (Characters cards component appearance on the Group detail page)

---
+++ src/app/cards-list/cards-list.component.ts
@@ -0,0 +1,43 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {CharacterService} from '../services/character.service';
+import {Router} from '@angular/router';
+import {Character} from '../models/character';
+import {Group} from '../models/groups';
+
+@Component({
+  selector: 'app-cards-list',
+  templateUrl: './cards-list.component.html',
+  styleUrls: ['./cards-list.component.css']
+})
+export class CardsListComponent implements OnInit {
+
+  constructor(private characterService: CharacterService) { }
+
+  @Input() cards: Character[] | number[];
+  @Input() group: Group;
+
+  ngOnInit(): void {
+    this.getCardsFromID();
+  }
+
+  // we can receive from the parent file 2 types of card : an array of objects Characters or of id numbers.
+  // if we receive an array of id, we need to convert it into an array of objects Characters
+  getCardsFromID(): any {
+    const group: Group = this.group;
+    // if we receive cards as Character[], we don't need to convert ids into characters
+    if (group === undefined) {
+      return;
+    }
+    console.log('searching for group members ');
+    this.characterService.getCardsFromID(group)
+      .subscribe(character => this.cards = character);
+  }
+
+  onOver(character: EventTarget): void {
+    this.characterService.Mouseover(character);
+  }
+
+  onOut(character: EventTarget): void {
+    this.characterService.Mouseout(character);
+  }
+}
